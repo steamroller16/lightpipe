bbd40eb9ae227844b1bb6d1a37ca11175e103b28
tikiman098 <tikiman098@gmail.com>
2014-03-02 01:11:44 -0600

    birth of the new codebase :D

---
+++ code/src/main.c
@@ -0,0 +1,42 @@
+
+
+
+/*
+-------------------------------------------
+Peripherals:
+
+ADC
+	Read battery voltage
+		Adjust behavior to maximize battery life
+		Warn user of imminent battery depletion
+		Adjust battery fuel gauge LED color
+	Read light sensor voltage
+		Adjust behavior in accordance with ambient lighting
+
+I2C
+	Accelerometer (w/ interrupt)
+	Battery charger (w/ interrupt)
+	Signal/brake LEDs
+
+UART
+	USB connectivity
+
+GPIO (Inputs)
+	Touch sensors (x3)
+
+GPIO (Outputs)
+	Control light sensor gain
+	Speaker
+	General RGB LED (x3)
+	Vibrator
+
+Debug (JTAG)
+	2-wire TCK, TDIO
+
+-------------------------------------------
+*/
+
+
+
+
+
