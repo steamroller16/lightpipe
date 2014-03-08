51b7618f17c4cf2980139ec4c48634255def3169
tikiman098 <tikiman098@gmail.com>
2014-03-07 23:15:54 -0600

    code plan update with pins and addresses

--- code/src/main.c
+++ code/src/main.c
@@ -6,39 +6,48 @@
 Peripherals:
 
 ADC
-	Read battery voltage
+	[A0] Read battery voltage
 		Adjust behavior to maximize battery life
 		Warn user of imminent battery depletion
 		Adjust battery fuel gauge LED color
-	Read light sensor voltage
+	[A3] Read light sensor voltage ()
 		Adjust behavior in accordance with ambient lighting
 
-I2C
-	Accelerometer (w/ interrupt)
-	Battery charger (w/ interrupt)
-	Signal/brake LEDs
+I2C [UCB0SCL, UCB0SDA]
+	[P2.0] Accelerometer (w/ interrupt)
+	[P2.5] Battery charger (w/ interrupt)
+	[1000 001x] Signal LEDs (front)
+	[1000 010x] Signal LEDs (back)
+	[1000 011x] Brake LEDs (back)
 
-UART
+UART [UCA0RXD, UCA0TXD]
 	USB connectivity
 
 GPIO (Inputs)
-	Touch sensors (x3)
+	[P2.1, P2.2, P2.3] Touch sensors (x3)
 
 GPIO (Outputs)
-	Control light sensor gain (x2)
-	Speaker
-	General RGB LED color/brightness (x3)
-	Vibrator
+	[P2.6, P2.7] Control light sensor gain (x2)
+	[P2.4] Speaker
+	[P3.4, P3.5, P3.6] General RGB LED color (R, G, B)
+	[P3.1, P3.2, P3.3] General RGB LED select (x3)
+	[P3.7] Vibrator
 
 Debug (JTAG)
-	2-wire TCK, TDIO
+	[SBWTDIO, SBWTCK] 2-wire TCK, TDIO
 
 -------------------------------------------
 Use vertical acceleration to synchronize flashing?
 
 */
 
+#include <msp430.h>
 
+init_main(void)
+{
+	// Stop the watch dog timer
+	WDTCTL = WDTPW + WDTHOLD;
+	Call the various other init's
+}
 
-
-
+init_
