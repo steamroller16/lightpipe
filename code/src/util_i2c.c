e79b85969a75b88ac72db197d6756ea6f8df90dd
tikiman098 <tikiman098@gmail.com>
2014-06-23 20:33:35 -0500

    I2C Write demo w/ I2C LED's
    Works with older code (committed here). Trying to figure out why my new
    code (commented - which I think is cleaner) doesn't work. Problem with
    writing has been isolated to I2C ISR. Also, figured out that the way to
    turn the LED's on and off is with their LEDMODE bits, rather than by
    turning on/off the MODE1 oscillator bit. The latter results in glitches
    during operation.

--- code/src/util_i2c.c
+++ code/src/util_i2c.c
@@ -7,6 +7,167 @@
 // ----------------------------------------------------------
 // Private Global Variables
 // ----------------------------------------------------------
+static int TxByteCounter;
+static int TxByteLength;
+static char *TxBytePointer;
+static int TxSendStopCondition;
+
+// ----------------------------------------------------------
+// Function Implementations
+// ----------------------------------------------------------
+void util_i2c_init(void)
+{
+// ----Set UCB0CTL1----
+// Perform UCB0 software reset
+UCB0CTL1 |= UCSWRST;
+
+// ----Set UCB0CTL0----
+// Set UCB0 to be Master -> UCMST
+// Set UCB0 to use I2C protocol -> UCMODE_3
+// Set UCB0 to use synchronous comm -> UCSYNC
+UCB0CTL0 = UCMST + UCMODE_3 + UCSYNC;
+
+// ----Set UCB0CTL1----
+// Set UCB0 to use SMCLK -> UCSSEL_2
+// Set UCB0 to transmitter mode -> UCTR
+// Maintain software reset -> UCSWRST
+UCB0CTL1 = UCSSEL_2 + UCTR + UCSWRST;
+
+// ----Set UCB0BRO, UCB0BR1----
+// Set UCB0 clock divider to 12
+// fSCL = SMCLK/12 = ~100kHz
+UCB0BR0 = 12;
+UCB0BR1 = 0;
+
+// TODO: may need to init smclk somewhere else
+
+// TEST: SET SLAVE ADR HERE
+UCB0I2CSA = I2C_SLAVE_ADR_LED_ALL_CALL;
+// UCB0I2CSA = I2C_SLAVE_ADR_LED_FRONT_SIGNAL;
+// UCB0I2CSA = I2C_SLAVE_ADR_LED_REAR_SIGNAL;
+// UCB0I2CSA = I2C_SLAVE_ADR_LED_REAR_BRAKE;
+// Clear SW reset, resume operation
+UCB0CTL1 &= ~UCSWRST;
+
+// May or may not need this
+// ----Set IE2----
+// Enable USCI_B0 receive interrupt
+// IE2 |= UCB0RXIE;
+
+// Enable not-acknowledge interrupt
+UCB0I2CIE |= UCNACKIE;
+
+// Enable USCI_B0 transmit interrupt
+IE2 |= UCB0TXIE;
+}
+
+void util_i2c_set_slave_adr(unsigned int slave_adr)
+{
+UCB0I2CSA = slave_adr;
+}
+
+void util_i2c_write(char *msg, int length, int send_stop_condition)
+{
+TxByteCounter = 0;
+TxByteLength = length;
+TxBytePointer = msg;
+TxSendStopCondition = send_stop_condition;
+
+// I2C TX, start condition
+// // // // // // // // UCB0CTL1 |= UCTR + UCTXSTT;
+
+// WORKS.--
+// Set UCB0 to I2C Transmitter Mode
+UCB0CTL1 |= UCTR;
+// Send Start condition
+UCB0CTL1 |= UCTXSTT;
+// --------
+
+
+
+// Go to sleep
+__bis_SR_register(CPUOFF + GIE);
+}
+
+void util_i2c_read(char *msg, int length)
+{
+// I2C RX, start condition
+UCB0CTL1 &= ~UCTR;
+UCB0CTL1 |= UCTXSTT;
+
+// Wait for reception to complete
+while (UCB0CTL1 & UCTXSTT);
+
+// Get msg from rx buffer
+*msg = UCB0RXBUF;
+}
+
+#pragma vector = USCIAB0TX_VECTOR
+__interrupt void USCIAB0TX_ISR(void)
+{
+	// Check TX byte counter
+	if (TxByteCounter < TxByteLength)
+	{
+		// Load TX buffer
+		UCB0TXBUF = TxBytePointer[TxByteCounter];
+
+		// Increment TX byte counter
+		TxByteCounter++;
+	}
+	else
+	{
+		if (TxSendStopCondition)
+		{
+		// I2C stop condition
+		UCB0CTL1 |= UCTXSTP;
+		}
+
+		// Clear USCI_B0 TX int flag
+		IFG2 &= ~UCB0TXIFG;
+
+		// Exit LPM0
+		__bic_SR_register_on_exit(CPUOFF);
+	}
+}
+/*{
+	if(TxByteLength > 0)
+	{
+		// Load TX buffer
+		UCB0TXBUF = TxBytePointer[TxByteCounter];
+
+		// Increment TX byte counter
+		TxByteCounter++;
+
+		// Check TX byte counter
+		if (TxByteCounter == TxByteLength)
+		{
+			if (TxSendStopCondition)
+			{
+				// I2C stop condition
+				UCB0CTL1 |= UCTXSTP;
+			}
+			
+			IFG2 &= ~UCB0TXIFG;
+			
+			// Exit LPM0
+			__bic_SR_register_on_exit(CPUOFF);
+		}
+	}
+	//UCB0TXIFG automatically reset after writing to TX buffer
+}*/
+
+
+
+/*
+//-----------------------------------------------------------
+// util_i2c.c
+//-----------------------------------------------------------
+// Utility: Functions to use I2C communication bus
+//-----------------------------------------------------------
+#include "util_i2c.h"
+// ----------------------------------------------------------
+// Private Global Variables
+// ----------------------------------------------------------
 static int 		TxByteCounter = 0;
 static int 		TxByteLength = 0;
 static char 	*TxBytePointer;
@@ -176,3 +337,4 @@ __interrupt void USCIAB0RX_ISR(void)
 	}
 	//UCB0RXIFG automatically reset after reading from RX buffer
 }
+*/
