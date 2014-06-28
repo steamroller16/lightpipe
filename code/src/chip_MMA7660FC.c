ac7b590b872acedd1d4a6edbd7b266ae94fcefea
tikiman098 <tikiman098@gmail.com>
2014-06-28 18:38:47 -0500

    finished comms w/ accel
    still needs testing

--- code/src/chip_MMA7660FC.c
+++ code/src/chip_MMA7660FC.c
@@ -21,26 +21,43 @@ void chip_MMA7660FC_init(void)
 {
 // Set Address to the Accelerometer
 util_i2c_set_slave_adr(I2C_SLAVE_ADR_ACCEL);
+
+chip_MMA7660FC_tx_length = 5;
+chip_MMA7660FC_tx_buffer[0] = (I2C_MMA7660FC_REG_SPCNT);
 // -----------------------
-// Successful tap detection causes an interrupt
-// Exiting Auto-sleep causes an interrupt
+// I2C_MMA7660FC_REG_SPCNT
+// Set the number of counts of inactivity before entering sleep mode (0-255)counts)
+// -----------------------
+chip_MMA7660FC_tx_buffer[1] = (128);
+// -----------------------
+// I2C_MMA7660FC_REG_INTSU
+// Exiting Auto-sleep causes an interrupt (Proc. would shutdown when this interrupt is received)
 // Set automatic interrupt to fire after every accel update of XOUT, YOUT, ZOUT
 // -----------------------
+chip_MMA7660FC_tx_buffer[2] = (
+I2C_MMA7660FC_ASINT +
+I2C_MMA7660FC_GINT
+);
+// -----------------------
+// I2C_MMA7660FC_REG_MODE
 // Set mode to active
 // Auto-wake: Enabled
 // Auto-sleep: Enabled
 // -----------------------
-chip_MMA7660FC_tx_length = 2;
-chip_MMA7660FC_tx_buffer[0] = (I2C_MMA7660FC_REG_INTSU);
-chip_MMA7660FC_tx_buffer[1] = (
-I2C_MMA7660FC_PDINT +
-I2C_MMA7660FC_ASINT +
-I2C_MMA7660FC_GINT
-);
-chip_MMA7660FC_tx_buffer[2] = (
+chip_MMA7660FC_tx_buffer[3] = (
 I2C_MMA7660FC_MODE +
 I2C_MMA7660FC_AWE + 
 I2C_MMA7660FC_ASE
+);
+// -----------------------
+// I2C_MMA7660FC_REG_SR
+// Set Auto-sleep (active) sample rate: 32 samples/sec
+// Set Auto-wake (sleeping) sample rate: 1 sample/sec
+// -----------------------
+chip_MMA7660FC_tx_buffer[4] = (
+I2C_MMA7660FC_AMSR_1 + 
+I2C_MMA7660FC_AWSR_1 + 
+I2C_MMA7660FC_AWSR_0
 );
 // Transmit command array to device
 util_i2c_write(
@@ -48,45 +65,6 @@ util_i2c_write(
 	chip_MMA7660FC_tx_length,
 	I2C_SEND_STOP
 );
-
-// -----------------------
-// Set value sleep counter must count to in order to wake up
-// -----------------------
-
-
-
-
-
-// // -----------------------
-// // Set all LED's control mode to PWM
-// chip_MMA7660FC_tx_length = 3;
-// chip_MMA7660FC_tx_buffer[0] = (I2C_MMA7660FC_CMD_AUTO_INC_ALL + I2C_MMA7660FC_REG_LEDOUT0);
-// chip_MMA7660FC_tx_buffer[1] = 0xAA;
-// chip_MMA7660FC_tx_buffer[2] = 0xAA;
-// // Transmit command array to device
-// util_i2c_write(
-	// chip_MMA7660FC_tx_buffer,
-	// chip_MMA7660FC_tx_length,
-	// I2C_SEND_STOP
-// );
-// // -----------------------
-// // Set all LED's Dutycycle to 0%
-// chip_MMA7660FC_tx_length = 9;
-// chip_MMA7660FC_tx_buffer[0] = (I2C_MMA7660FC_CMD_AUTO_INC_ALL + I2C_MMA7660FC_REG_PWM0);
-// chip_MMA7660FC_tx_buffer[1] = 0x00;
-// chip_MMA7660FC_tx_buffer[2] = 0x00;
-// chip_MMA7660FC_tx_buffer[3] = 0x00;
-// chip_MMA7660FC_tx_buffer[4] = 0x00;
-// chip_MMA7660FC_tx_buffer[5] = 0x00;
-// chip_MMA7660FC_tx_buffer[6] = 0x00;
-// chip_MMA7660FC_tx_buffer[7] = 0x00;
-// chip_MMA7660FC_tx_buffer[8] = 0x00;
-// // Transmit command array to device
-// util_i2c_write(
-	// chip_MMA7660FC_tx_buffer,
-	// chip_MMA7660FC_tx_length,
-	// I2C_SEND_STOP
-// );
 }
 void chip_MMA7660FC_read( char *result, int length, unsigned int device_reg )
 {
