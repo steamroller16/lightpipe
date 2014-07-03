439883975a4ca0a226d2e02fce3e8ca0c182eba7
tikiman098 <tikiman098@gmail.com>
2014-07-03 00:30:53 -0500

    started working on algorithm
    made a new file, barely starting out

--- code/src/analyze_accel.c
+++ code/src/analyze_accel.c
@@ -1,8 +1,38 @@
 //-----------------------------------------------------------
 //-----------------------------------------------------------
-// sensor_accelerometer.c
+// analyze_accel.c
 //-----------------------------------------------------------
 // accelerometer: each reading, check to see what the acceleration is [-x dir for this geometry]. if the forward acceleration is greater than a preset threshold, then the bike is starting from a stop. When a person stops, the acceleration goes negative, goes to zero, goes positive, then back to zero (assuming no further oscillations). Therefore, the setting of that threshold prevents spurious brake light flickering at a stop.
+char analyze_accel_xyz[3];
+
+signed int analyze_accel (signed int )
+
+// Calculate a rolling average over a long time interval.
+// This long average provides the baseline acceleration.
+int baseline = 0;
+
+//	Acceleration greater than a threshold, measured from the baseline, indicates pedaling
+int delta_pedal = 0.1
+
+// 	Acceleration less than a different threshold, measured  from the baseline indicates braking
+int delta_brake = 0.1
+
+
+if (baseline)
+
+// Calculate a rolling average over a short timer interval.
+// This short average 
+// If the short average differs from the long average by greater than a threshold, then 
+
+
+
++
+
+
+
+
+
+
 sensor_accelerometer_init
 
 sensor_accelerometer_read_x(void)
